655576c39ce3daf2795454714b5be62102ffcf30
DmitriiG8421 <golikov.dima15@gmail.com>
2026-05-16 11:35:24 +1000

    added some code

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+#Flask creates the web application
+#render_template loads the HMTL file
+#request gets user input from the form
+import requests
+
+#sends HTTP requests to OpenWeather (chef) API (waiting staff)
+import os
+
+#reads env data (environment variables)
+
+from dotenv import load_dotenv
+#loads values from the .env file
+
+#step-1:
+
+load_dotenv()
+
+#step-2: start creating the web app
+
+app = Flask(__name__)
+
+#step-3: GET the API key from the .env
+
+API_KEY = os.getenv("API_KEY")
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+#https://api.openweathermap.org/data/2.5/weather?q=New York&appid=API_KEY&units=imperial
+
+city = "Sydney"
+
+#parameters can be stored as a dictionary
+
+data = {
+    "q":city,
+    "appid":API_KEY,
+    "units":"metric"
+}
+
+data_received = requests.get(url,params=data)
+
+json_data = data_received.json()
+print(json_data)
+print("City:",json_data["name"])
---
+++ weather_data_web_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+API_KEY = os.getenv("API_KEY")
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+app = Flask(__name__) #creates the Flask website app
+@app.route("/") #decorator
+def home():
+    city = "Sydney"
+    data = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    data_received = requests.get(url, params=data)
+    json_data = data_received.json()
+
+    return f"""
+    <h1> Weather Data for {json_data["name"]} </h1>
+    <p> The temperature is {json_data['main']['temp']} </p>
+    <p> The humidity is {json_data['main']['humidity']}% </p>
+    <p> The wind speed is {json_data['wind']['speed']} </p>
+    """
+app.run(debug=True)
