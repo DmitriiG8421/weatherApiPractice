bd8b7551b653fd2b250c223d78cfb278211fb861
DmitriiG8421 <golikov.dima15@gmail.com>
2026-05-30 12:11:08 +1000

    Finished posting and created a city parramater to the code

--- weather_post.py
+++ weather_post.py
@@ -1,36 +1,45 @@
-from crypt import methods
-
 from flask import Flask, render_template, request
+# Flask creates the web application
+# render_template loads the HMTL file
+# request gets user input from the form
 import requests
+
+# sends HTTP requests to OpenWeather (chef) API (waiting staff)
 import os
+
+# reads env data (environment variables)
+
 from dotenv import load_dotenv
 
+# loads values from the .env file
 load_dotenv()
-app = Flask(__name__)
+
+app = Flask(__name__)  # creates the Flask website app
+
 API_KEY = os.getenv("API_KEY")
 
 url = "https://api.openweathermap.org/data/2.5/weather"
 
-app = Flask(__name__) #creates the Flask website app
-@app.route("/",methods=["GET","POST","PATCH"]) #decorator
+
+@app.route("/", methods=["GET", "POST", "PATCH"])  # decorator
 def home():
+    username = ""
+    city = ""
     if request.method == "POST":
         username = request.form.get("username")
-
-
-
-    data = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric"
-    }
-    data_received = requests.get(url, params=data)
-    json_data = data_received.json()
+        city = request.form.get("city")
 
     return f"""
-    <h1> Weather Data for {json_data['sys']['name']} </h1>
-    <p> The temperature is {json_data['main']['temp']} </p>
-    <p> The humidity is {json_data['main']['humidity']}% </p>
-    <p> The wind speed is {json_data['wind']['speed']} </p>
+    <h1>My First Flask Website</h1>
+    <form method="POST">
+        <input type="text" name="username" placeholder="Enter your user name">
+            <button type="submit">Submit</button>
+        <input type="text" name="city" placeholder="Enter your city name">
+            <button type="submit">Submit</button>
+    </form>
+    <p>Your username is {username}</p>
+    <p>Your city is {city}</p>
     """
+
+
 app.run(debug=True)
