48f4708e1e46f3d2673dab01ed35ab51b8e83676
DmitriiG8421 <golikov.dima15@gmail.com>
2026-06-13 10:33:58 +1000

    something

--- weather_post.py
+++ weather_post.py
@@ -13,18 +13,14 @@ from dotenv import load_dotenv
 
 # loads values from the .env file
 load_dotenv()
-
 app = Flask(__name__)  # creates the Flask website app
-
 API_KEY = os.getenv("API_KEY")
-
 url = "https://api.openweathermap.org/data/2.5/weather"
-
 
 @app.route("/", methods=["GET", "POST", "PATCH"])  # decorator
 def home():
-    username = ""
     city = ""
+    json_data = []
     if request.method == "POST":
         username = request.form.get("username")
         city = request.form.get("city")
@@ -33,12 +29,14 @@ def home():
     <h1>My First Flask Website</h1>
     <form method="POST">
         <input type="text" name="username" placeholder="Enter your user name">
-            <button type="submit">Submit</button>
         <input type="text" name="city" placeholder="Enter your city name">
             <button type="submit">Submit</button>
     </form>
     <p>Your username is {username}</p>
     <p>Your city is {city}</p>
+    <p> The temperature is {json_data['main']['temp']} </p>
+    <p> The humidity is {json_data['main']['humidity']}% </p>
+    <p> The wind speed is {json_data['wind']['speed']} </p>
     """
 
 
